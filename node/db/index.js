a3491a2f397f6144f9f3ee492c91af02c2965c2a
xuejiahao <763618549@qq.com>
2021-03-25 18:08:17 +0800

    数据库配置更改

--- node/db/index.js
+++ node/db/index.js
@@ -1,9 +1,9 @@
 const mysql = require("mysql")
 module.exports = mysql.createConnection({
-  host: "localhost",
+  host: "http://121.36.32.228",
   port: 3306,
-  user: "root",
-  password: "root",
+  user: "campus_health",
+  password: "22Sj8KKrb43Rn2nC",
   multipleStatements: true,
   database: "campus_health",
 })
